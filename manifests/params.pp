6b7f60421d4da18502f6b03931b915243e59d491
Ohad Levy <ohadlevy@redhat.com>
2012-03-04 10:37:54 +0200

    adds basic cron handling for puppet runs

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,5 +11,6 @@ class puppet::params {
   $apache_conf_dir     = $foreman::params::apache_conf_dir
   $app_root            = "${dir}/rack"
   $ssl_dir             = '/var/lib/puppet/ssl'
-
+  $cron_range          = 60 # the maximum value for our cron
+  $cron_interval       = 2  # the amount of values within the $cron_range
 }
