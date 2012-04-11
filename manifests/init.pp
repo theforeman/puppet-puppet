f7ae6195dcd5c342142b84fafe71e542fa69b8f5
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2012-04-11 21:29:08 +0100

    Don't make the choice of daemon or cron a global one

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,4 @@ class puppet {
   include puppet::params
   include puppet::install
   include puppet::config
-
-  case $puppet::params::run_style {
-    'cron': { include puppet::cron }
-    'daemon': { include puppet::daemon }
-    default: { }
-  }
 }
