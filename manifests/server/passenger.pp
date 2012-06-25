c89c4af604f75af8fdb85daf6f60761eb9c86830
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2012-06-25 23:44:48 +0100

    Stop puppetmaster ever starting on a Debian Passenger install

--- manifests/server/passenger.pp
+++ manifests/server/passenger.pp
@@ -3,6 +3,15 @@ class puppet::server::passenger {
   include apache::params
   include ::passenger
 
+  case $::operatingsystem {
+    Debian,Ubuntu: {
+      file { '/etc/default/puppetmaster':
+        content => "START=no\n",
+        before  => Class['puppet::server::install']
+      }
+    }
+  }
+
   file {'puppet_vhost':
     path    => "${apache::params::configdir}/puppet.conf",
     content => template('puppet/server/puppet-vhost.conf.erb'),
