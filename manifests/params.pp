a606073be077816285d5c2e7f6678da5b4c6a4a8
Dominic Cleal <dcleal@redhat.com>
2013-01-02 14:44:03 +0000

    refs #2065 - provide version dependent puppetrun/kick path

--- manifests/params.pp
+++ manifests/params.pp
@@ -57,9 +57,11 @@ class puppet::params {
   if versioncmp($puppetversion, '3.0') < 0 {
     $puppetca_path = '/usr/sbin'
     $puppetca_bin  = 'puppetca'
+    $puppetrun_cmd = '/usr/sbin/puppetrun'
   } else {
     $puppetca_path = '/usr/bin'
     $puppetca_bin = 'puppet cert'
+    $puppetrun_cmd = '/usr/bin/puppet kick'
   }
 
   $puppetca_cmd = "${puppetca_path}/${puppetca_bin}"
