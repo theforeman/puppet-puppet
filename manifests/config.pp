6694290a96ace21d052de253165d615bfff65dbf
Dominic Cleal <dcleal@redhat.com>
2013-01-02 13:58:12 +0000

    Add 'listen' support for puppet agent

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,4 +7,13 @@ class puppet::config {
     content => template($puppet::agent_template),
   }
 
+  file { "${puppet::dir}/auth.conf":
+    content => template($puppet::auth_template),
+  }
+
+  if $puppet::listen {
+    file { "${puppet::dir}/namespaceauth.conf":
+      content => template($puppet::nsauth_template),
+    }
+  }
 }
