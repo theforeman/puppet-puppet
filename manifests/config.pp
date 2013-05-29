06de913912d455c2dc242060bf0a3a34fe6c5651
Dominic Cleal <dcleal@redhat.com>
2013-05-29 11:12:03 +0100

    Ensure the Puppet config is deployed before the agent is started

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,32 +1,32 @@
 # Set up the puppet config
 class puppet::config {
+  case $::puppet::runmode {
+    'service': {
+      $runmode_class = 'daemon'
+    }
+    'cron': {
+      $runmode_class = 'cron'
+    }
+    default: {
+      fail("Runmode of ${puppet::runmode} not supported by puppet::config!")
+    }
+  }
+
   file { $puppet::dir:
     ensure => directory,
-  }
-
+  } ->
   file { "${puppet::dir}/puppet.conf":
     content => template($puppet::agent_template),
-  }
-
+  } ->
   file { "${puppet::dir}/auth.conf":
     content => template($puppet::auth_template),
-  }
+  } ->
+  class { "::puppet::${runmode_class}": }
 
   if $puppet::listen {
     file { "${puppet::dir}/namespaceauth.conf":
       content => template($puppet::nsauth_template),
-    }
-  }
-
-  case $::puppet::runmode {
-    'service': {
-      include ::puppet::daemon
-    }
-    'cron': {
-      include ::puppet::cron
-    }
-    default: {
-      fail("Runmode of ${puppet::runmode} not supported by puppet::config!")
+      before  => Class["::puppet::${runmode_class}"],
     }
   }
 }
