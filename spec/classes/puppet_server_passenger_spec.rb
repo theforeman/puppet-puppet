561ee648279cc01a5302699db1f92f7636c4129b
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2013-07-19 12:09:51 +0200

    Add more tests

---
+++ spec/classes/puppet_server_passenger_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'puppet::server::passenger' do
+  let :facts do
+    {
+      :osfamily   => 'RedHat',
+      :fqdn       => 'puppetmaster.example.com',
+      :clientcert => 'puppetmaster.example.com',
+    }
+  end
+
+  describe 'with no custom parameters' do
+    let :pre_condition do
+      "
+      include puppet
+      class {'puppet::server':}
+      "
+    end
+
+    it 'should include the puppet vhost' do
+      should contain_file('puppet_vhost').
+        with_content(/^Listen 8140$/).
+        with_content(/^<VirtualHost \*:8140>$/).
+        with_content(/^  SSLCertificateFile\s+\/var\/lib\/puppet\/ssl\/certs\/#{facts[:fqdn]}.pem$/).
+        with_content(/^  SSLCertificateKeyFile\s+\/var\/lib\/puppet\/ssl\/private_keys\/#{facts[:fqdn]}.pem$/).
+        with_content(/^  SSLCACertificateFile\s+\/var\/lib\/puppet\/ssl\/ca\/ca_crt.pem$/).
+        with_content(/^  SSLCertificateChainFile\s+\/var\/lib\/puppet\/ssl\/ca\/ca_crt.pem$/).
+        with_content(/^  SSLCARevocationFile\s+\/var\/lib\/puppet\/ssl\/ca\/ca_crl.pem$/).
+        with_content(/^  DocumentRoot \/etc\/puppet\/rack\/public\/$/).
+        with_content(/^  <Directory \/etc\/puppet\/rack>$/).
+        with({
+          :path    => '/etc/httpd/conf.d/puppet.conf',
+          :mode    => '0644',
+          :notify  => 'Exec[reload-apache]',
+          :before  => 'Service[httpd]',
+          :require => 'Class[Puppet::Server::Rack]',
+        })
+    end
+  end
+
+end
---
+++ spec/classes/puppet_server_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'puppet::server' do
+
+  let :pre_condition do
+    "include puppet"
+  end
+
+  let :facts do
+    {
+      :osfamily   => 'RedHat',
+      :fqdn       => 'puppetmaster.example.com',
+      :clientcert => 'puppetmaster.example.com',
+    }
+  end
+
+  describe 'with no custom parameters' do
+    it 'should include classes' do
+      should include_class('puppet::server::install')
+      should include_class('puppet::server::config')
+      should include_class('puppet::server::service')
+    end
+  end
+
+end
