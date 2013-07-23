71278b9f5e644ebbe520cf6736f227ccdad4c76a
Lukas Zapletal <lzap+git@redhat.com>
2013-07-23 15:00:36 +0200

    adding support for puppetagent service name in F19

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,7 @@
 # Set up the puppet client as a service
 class puppet::service {
   service {'puppet':
+    name    => $puppet::params::service_name,
     require => Class['puppet::install']
   }
 }
