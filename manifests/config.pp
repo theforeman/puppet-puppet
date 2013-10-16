9c60a8f923bdc2088bd522d207d186f664b64d51
Dominic Cleal <dcleal@redhat.com>
2013-10-16 19:48:01 +0100

    Notify agent service when configs change

--- manifests/config.pp
+++ manifests/config.pp
@@ -18,16 +18,17 @@ class puppet::config {
   } ->
   file { "${puppet::dir}/puppet.conf":
     content => template($puppet::agent_template),
-  } ->
+  } ~>
   file { "${puppet::dir}/auth.conf":
     content => template($puppet::auth_template),
-  } ->
-  class { "::puppet::${runmode_class}": }
+  } ~>
+  class { "::puppet::${runmode_class}": } ~>
+  Class['::puppet::service']
 
   if $puppet::listen {
     file { "${puppet::dir}/namespaceauth.conf":
       content => template($puppet::nsauth_template),
-      before  => Class["::puppet::${runmode_class}"],
+      notify  => Class["::puppet::${runmode_class}", '::puppet::service'],
     }
   }
 }
