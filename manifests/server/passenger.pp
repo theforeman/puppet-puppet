488e069c1687605e909b9920e94d21990ac58232
Rene Visser <rene@rbvisser.nl>
2014-01-24 14:16:00 +0100

    include apache::mod::passenger
    for deploying a puppetmaster without theforman  you also need  mod_passenger

--- manifests/server/passenger.pp
+++ manifests/server/passenger.pp
@@ -16,6 +16,7 @@ class puppet::server::passenger (
 ) {
   include ::puppet::server::rack
   include ::apache
+  include ::apache::mod::passenger
 
   case $::operatingsystem {
     Debian,Ubuntu: {
