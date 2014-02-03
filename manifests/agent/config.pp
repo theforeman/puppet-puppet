212a933b37b246b6431323639485577aa8fa4522
Angel L. Mateo <amateo@um.es>
2014-02-03 14:50:02 +0100

    If runmode is 'service' and is a debian based system, ensure that /etc/default/puppet has START=yes

--- manifests/agent/config.pp
+++ manifests/agent/config.pp
@@ -2,4 +2,11 @@ class puppet::agent::config {
   concat_fragment { 'puppet.conf+20-agent':
     content => template($puppet::agent_template),
   }
+  
+  if $::puppet::runmode == 'service' and $::osfamily == 'Debian'{
+    augeas {'puppet::set_start':
+      context => "/files/etc/default/puppet",
+      changes => "set START yes",
+    }
+  }
 }
