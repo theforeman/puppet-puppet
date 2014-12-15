3fdeed75d23f146e196c04e9f83505e15cf2734d
Harry Danes <harry@danes.eu>
2014-12-15 20:20:55 +0100

    set mode for rack application directories
    The directories which are used by the rack application should be
    readable if the system is hardened with an umask of 0077 (instead
    of the default 0022).

--- manifests/server/rack.pp
+++ manifests/server/rack.pp
@@ -27,6 +27,7 @@ class puppet::server::rack {
     [$puppet::server_app_root, "${puppet::server_app_root}/public", "${puppet::server_app_root}/tmp"]:
       ensure => directory,
       owner  => $puppet::server_user,
+      mode   => '0755',
   }
 
   $configru_version = $::puppetversion ? {
