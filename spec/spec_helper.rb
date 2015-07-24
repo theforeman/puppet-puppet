f49283f0e44e12eb57e1b0abcf0f93286b04aa2c
Dominic Cleal <dcleal@redhat.com>
2015-07-24 11:29:34 +0100

    modulesync: add param docs lint check, add verify_* helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,22 @@ include RspecPuppetFacts
 class Undef
   def inspect; 'undef'; end
 end
+
+def get_content(subject, title)
+  content = subject.resource('file', title).send(:parameters)[:content]
+  content.split(/\n/).reject { |line| line =~ /(^#|^$|^\s+#)/ }
+end
+
+def verify_exact_contents(subject, title, expected_lines)
+  expect(get_content(subject, title)).to eq(expected_lines)
+end
+
+def verify_concat_fragment_contents(subject, title, expected_lines)
+  content = subject.resource('concat::fragment', title).send(:parameters)[:content]
+    (content.split("\n") & expected_lines).should == expected_lines
+end
+
+def verify_concat_fragment_exact_contents(subject, title, expected_lines)
+  content = subject.resource('concat::fragment', title).send(:parameters)[:content]
+    content.split(/\n/).reject { |line| line =~ /(^#|^$|^\s+#)/ }.should == expected_lines
+end
