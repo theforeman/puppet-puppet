12b1d5b014cdc2f564cbea121c620b3ec765b4e4
Dominic Cleal <dcleal@redhat.com>
2015-07-24 14:14:01 +0100

    Remove module's own verify_* spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 #   https://github.com/theforeman/foreman-installer-modulesync
 
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'lib/module_spec_helper'
 
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
