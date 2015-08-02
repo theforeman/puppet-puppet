theforeman/puppet-puppet

commit f74e45d1dc8a96929f22733f39052a798aeff52f

support puppet master setup on FreeBSD
Closes GH-288

manifests/params.pp
@@ -44,11 +44,12 @@ class puppet::params {
       # Windows prefixes normal paths with the Data Directory's path and leaves 'puppet' off the end
       $dir_prefix = 'C:/ProgramData/PuppetLabs/puppet'
 
-      $dir    = "${dir_prefix}/etc"
-      $logdir = "${dir_prefix}/var/log"
-      $rundir = "${dir_prefix}/var/run"
-      $ssldir = "${dir_prefix}/etc/ssl"
-      $vardir = "${dir_prefix}/var"
+      $dir        = "${dir_prefix}/etc"
+      $logdir     = "${dir_prefix}/var/log"
+      $rundir     = "${dir_prefix}/var/run"
+      $ssldir     = "${dir_prefix}/etc/ssl"
+      $vardir     = "${dir_prefix}/var"
+      $sharedir   = "${dir_prefix}/share"
       $root_group = undef
     }
 
@@ -58,6 +59,7 @@ class puppet::params {
       $rundir     = '/var/run/puppet'
       $ssldir     = '/var/puppet/ssl'
       $vardir     = '/var/puppet'
+      $sharedir   = '/usr/local/share/puppet'
       $root_group = undef
     }
 
@@ -67,6 +69,7 @@ class puppet::params {
       $rundir     = '/var/run/puppet'
       $ssldir     = '/var/lib/puppet/ssl'
       $vardir     = '/var/lib/puppet'
+      $sharedir   = '/usr/share/puppet'
       $root_group = undef
     }
   }
@@ -113,7 +116,7 @@ class puppet::params {
   $server_service_fallback    = true
   $server_passenger_max_pool  = 12
   $server_httpd_service       = 'httpd'
-  $server_external_nodes      = '/etc/puppet/node.rb'
+  $server_external_nodes      = "${dir}/node.rb"
   $server_enc_api             = 'v2'
   $server_report_api          = 'v2'
   $server_request_timeout     = 60
@@ -154,7 +157,7 @@ class puppet::params {
   # Where remains our manifests dir
   $server_manifest_path        = "${dir}/manifests"
   # Modules in this directory would be shared across all environments
-  $server_common_modules_path  = ["${server_envs_dir}/common", "${dir}/modules", '/usr/share/puppet/modules']
+  $server_common_modules_path  = ["${server_envs_dir}/common", "${dir}/modules", "${sharedir}/modules"]
 
   # Dynamic environments config, ignore if the git_repo is 'false'
   # Path to the repository
@@ -180,9 +183,9 @@ class puppet::params {
 
   $server_package     = undef
   $server_version     = undef
-  $client_package     = $::operatingsystem ? {
-    /(Debian|Ubuntu)/ => ['puppet-common','puppet'],
-    default           => ['puppet'],
+  $client_package     = $::osfamily ? {
+    'Debian' => ['puppet-common','puppet'],
+    default  => ['puppet'],
   }
 
   # Only use 'puppet cert' on versions where puppetca no longer exists
@@ -191,9 +194,15 @@ class puppet::params {
     $puppetca_bin  = 'puppetca'
     $puppetrun_cmd = '/usr/sbin/puppetrun'
   } else {
-    $puppetca_path = '/usr/bin'
+    $puppetca_path = $::osfamily ? {
+      /^(FreeBSD|DragonFly)$/ => '/usr/local/bin',
+      default                 => '/usr/bin'
+    }
     $puppetca_bin = 'puppet cert'
-    $puppetrun_cmd = '/usr/bin/puppet kick'
+    $puppetrun_cmd = $::osfamily ? {
+      /^(FreeBSD|DragonFly)$/ => '/usr/local/bin/puppet kick',
+      default                 => '/usr/bin/puppet/kick'
+    }
   }
 
   $puppetca_cmd = "${puppetca_path}/${puppetca_bin}"

manifests/init.pp
@@ -380,6 +380,7 @@ class puppet (
   $logdir                        = $puppet::params::logdir,
   $rundir                        = $puppet::params::rundir,
   $ssldir                        = $puppet::params::ssldir,
+  $sharedir                      = $puppet::params::sharedir,
   $package_provider              = $puppet::params::package_provider,
   $port                          = $puppet::params::port,
   $listen                        = $puppet::params::listen,

manifests/server/config.pp
@@ -118,7 +118,7 @@ class puppet::server::config inherits puppet::config {
 
   }
   elsif ! $puppet::server_dynamic_environments {
-    file { '/usr/share/puppet':
+    file { $puppet::sharedir:
       ensure => directory,
     }
 

manifests/server/install.pp
@@ -2,9 +2,10 @@
 class puppet::server::install {
 
   $server_package_default = $::puppet::server_implementation ? {
-    'master'       => $::operatingsystem ? {
-      /(Debian|Ubuntu)/ => ['puppetmaster-common','puppetmaster'],
-      default           => ['puppet-server'],
+    'master'       => $::osfamily ? {
+      'Debian'                => ['puppetmaster-common','puppetmaster'],
+      /^(FreeBSD|DragonFly)$/ => [],
+      default                 => ['puppet-server'],
     },
     'puppetserver' => 'puppetserver',
   }
@@ -21,8 +22,13 @@ class puppet::server::install {
       owner  => $puppet::server_user,
     }
 
+    $git_shell = $::osfamily ? {
+      /^(FreeBSD|DragonFly)$/ => '/usr/local/bin/git-shell',
+      default                 => '/usr/bin/git-shell'
+    }
+
     user { $puppet::server_user:
-      shell   => '/usr/bin/git-shell',
+      shell   => $git_shell,
       require => Class['::git::install'],
     }
   }

manifests/server/rack.pp
@@ -18,9 +18,10 @@ class puppet::server::rack(
   $vardir         = $::puppet::vardir,
 ) {
   exec {'puppet_server_rack-restart':
-    command     => "/bin/touch ${app_root}/tmp/restart.txt",
-    refreshonly => true,
+    command     => "touch ${app_root}/tmp/restart.txt",
     cwd         => $app_root,
+    path        => '/bin:/usr/bin',
+    refreshonly => true,
     require     => [
       Class['puppet::server::install'],
       File["${app_root}/tmp"]

spec/classes/puppet_config_spec.rb
@@ -210,7 +210,7 @@ describe 'puppet::config' do
         if Puppet.version >= '3.6'
           concat_fragment_content.concat([
             '    environmentpath  = /usr/local/etc/puppet/environments',
-            '    basemodulepath   = /usr/local/etc/puppet/environments/common:/usr/local/etc/puppet/modules:/usr/share/puppet/modules',
+            '    basemodulepath   = /usr/local/etc/puppet/environments/common:/usr/local/etc/puppet/modules:/usr/local/share/puppet/modules',
           ])
         end
         verify_concat_fragment_exact_contents(catalogue, 'puppet.conf+10-main', concat_fragment_content)
@@ -251,7 +251,7 @@ describe 'puppet::config' do
         if Puppet.version >= '3.6'
           concat_fragment_content.concat([
             '    environmentpath  = C:/ProgramData/PuppetLabs/puppet/etc/environments',
-            '    basemodulepath   = C:/ProgramData/PuppetLabs/puppet/etc/environments/common:C:/ProgramData/PuppetLabs/puppet/etc/modules:/usr/share/puppet/modules',
+            '    basemodulepath   = C:/ProgramData/PuppetLabs/puppet/etc/environments/common:C:/ProgramData/PuppetLabs/puppet/etc/modules:C:/ProgramData/PuppetLabs/puppet/share/modules',
           ])
         end
         verify_concat_fragment_exact_contents(catalogue, 'puppet.conf+10-main', concat_fragment_content)

spec/classes/puppet_server_rack_spec.rb
@@ -14,7 +14,8 @@ describe 'puppet::server::rack' do
 
       it 'should define Exec[puppet_server_rack-restart]' do
         should contain_exec('puppet_server_rack-restart').with({
-          :command      => '/bin/touch /etc/puppet/rack/tmp/restart.txt',
+          :command      => 'touch /etc/puppet/rack/tmp/restart.txt',
+          :path         => '/bin:/usr/bin',
           :refreshonly  => true,
           :cwd          => '/etc/puppet/rack',
           :require      => ['Class[Puppet::Server::Install]', 'File[/etc/puppet/rack/tmp]'],

commit 38e5b9a37487265837e20cd20f770c6e6f692f12

Correct $puppetrun_cmd on Linux
Regression introduced in f74e45d1dc8a96929f22733f39052a798aeff52f.

manifests/params.pp
@@ -201,7 +201,7 @@ class puppet::params {
     $puppetca_bin = 'puppet cert'
     $puppetrun_cmd = $::osfamily ? {
       /^(FreeBSD|DragonFly)$/ => '/usr/local/bin/puppet kick',
-      default                 => '/usr/bin/puppet/kick'
+      default                 => '/usr/bin/puppet kick'
     }
   }
 