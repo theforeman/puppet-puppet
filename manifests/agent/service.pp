3f68188c6a207dfb75fcdf1638d48ef4ae892f8f
Dominic Cleal <dcleal@redhat.com>
2015-11-26 14:27:16 +0000

    Contain service implementations inside puppet::agent::service
    Preserves ordering of the agent service(s) in relation to
    puppet::agent::config etc.
    
    Fixes GH-336

--- manifests/agent/service.pp
+++ manifests/agent/service.pp
@@ -31,15 +31,24 @@ class puppet::agent::service {
     fail("Runmode of ${puppet::runmode} not supported on ${::kernel} operating systems!")
   }
 
+  anchor { 'puppet::agent::service_start': }
+  anchor { 'puppet::agent::service_end': }
+
+  Anchor['puppet::agent::service_start'] ->
   class { '::puppet::agent::service::daemon':
     enabled => $service_enabled,
-  }
+  } ->
+  Anchor['puppet::agent::service_end']
 
+  Anchor['puppet::agent::service_start'] ->
   class { '::puppet::agent::service::systemd':
     enabled => $systemd_enabled,
-  }
+  } ->
+  Anchor['puppet::agent::service_end']
 
+  Anchor['puppet::agent::service_start'] ->
   class { '::puppet::agent::service::cron':
     enabled => $cron_enabled,
-  }
+  } ->
+  Anchor['puppet::agent::service_end']
 }
