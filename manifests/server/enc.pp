06dfd8ce4aa130d96300b61ab3a169b7de3c5fb0
Simon Peeters <simon@inuits.eu>
2016-03-25 13:15:35 +0100

    Put the enc config in a separate class
    This makes it possible to "autodiscover" the enc using exported
    resources.

---
+++ manifests/server/enc.pp
@@ -0,0 +1,8 @@
+class puppet::server::enc(
+  $enc_path = $::puppet::server::external_nodes
+) {
+  puppet::config::master {
+    'external_nodes':     value => $enc_path;
+    'node_terminus':      value => 'exec';
+  }
+}
