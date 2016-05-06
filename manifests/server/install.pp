4fb540a150f8e24699b3a3bf46a2c638cf02518f
Dominic Cleal <dominic@cleal.org>
2016-05-06 10:34:42 +0100

    refs #14942 - order install before foreman/foreman_proxy for puppet group

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,6 +1,15 @@
 # Install the puppet server
 class puppet::server::install {
 
+  # Mirror the relationship, as defined() is parse-order dependent
+  # Ensures 'puppet' user group is present before managing users
+  if defined(Class['foreman_proxy::config']) {
+    Class['puppet::server::install'] -> Class['foreman_proxy::config']
+  }
+  if defined(Class['foreman::config']) {
+    Class['puppet::server::install'] -> Class['foreman::config']
+  }
+
   if $::puppet::manage_packages == true or $::puppet::manage_packages == 'server' {
     $server_package_default = $::puppet::server::implementation ? {
       'master'       => $::osfamily ? {
