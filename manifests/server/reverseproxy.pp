87db8d36e927603940eba7a2a252f7ec0e8f19db
superfantasticawesome <hello@superfantasticawesome.com>
2016-10-01 21:39:07 -0400

    Update reverseproxy.pp

--- manifests/server/reverseproxy.pp
+++ manifests/server/reverseproxy.pp
@@ -52,14 +52,14 @@ class puppet::server::reverseproxy (
     ],
     proxy_pass_match     => [
       {
-        'path'           => '^/.*/certificate.*/',
-        'url'            => "https://${ca_server}:${ca_port}",
-        'reverse_urls'   => "https://${ca_server}:${ca_port}",
+         'path'          => '^/.*/certificate.*/',
+          'url'          => "https://${ca_server}:${ca_port}",
+          'reverse_urls' => "https://${ca_server}:${ca_port}",
       },
       {
-        'path'           => '/',
-        'url'            => "http://localhost:${http_port}",
-        'reverse_urls'   => "http://localhost:${http_port}",
+          'path'         => '/',
+          'url'          => "http://localhost:${http_port}",
+          'reverse_urls' => "http://localhost:${http_port}",
       },
     ],
     require              => File["$confdir/public"],
