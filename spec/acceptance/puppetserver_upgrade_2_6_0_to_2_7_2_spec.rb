b589490874587e654d28e7a395bdff07e9e83854
Ewoud Kohl van Wijngaarden <github@kohlvanwijngaarden.nl>
2017-03-23 23:45:26 +0100

    Update acceptance tests
    This uses the modulesynced changes and adds tests on Debian

--- spec/acceptance/puppetserver_upgrade_2_6_0_to_2_7_2_spec.rb
+++ spec/acceptance/puppetserver_upgrade_2_6_0_to_2_7_2_spec.rb
@@ -3,17 +3,23 @@ require 'spec_helper_acceptance'
 describe 'Scenario: 2.6.0 to 2.7.2 upgrade:' do
   before(:context) do
     if check_for_package(default, 'puppetserver')
-      on(
-        default,
-        'rpm --erase puppetserver && ' \
-        'rm -rf /etc/sysconfig/puppetserver /etc/puppetlabs/puppetserver && ' \
-        'find /etc/puppetlabs/puppet/ssl/ -type f -delete'
-      )
+      on default, puppet('resource package puppetserver ensure=absent')
+      on default, 'rm -rf /etc/sysconfig/puppetserver /etc/puppetlabs/puppetserver'
+      on default, 'find /etc/puppetlabs/puppet/ssl/ -type f -delete'
     end
 
     # puppetserver won't start with lower than 2GB memory
     memoryfree_mb = fact('memoryfree_mb').to_i
     raise 'At least 2048MB free memory required' if memoryfree_mb < 256
+  end
+
+  case fact('osfamily')
+  when 'Debian'
+    from_version = '2.6.0-1puppetlabs1'
+    to_version = '2.7.2-1puppetlabs1'
+  else
+    from_version = '2.6.0'
+    to_version = '2.7.2'
   end
 
   context 'install 2.6.0' do
@@ -24,7 +30,7 @@ describe 'Scenario: 2.6.0 to 2.7.2 upgrade:' do
         server_foreman        => false,
         server_reports        => 'store',
         server_external_nodes => '',
-        server_version        => '2.6.0',
+        server_version        => '#{from_version}',
         # only for install test - don't think to use this in production!
         # https://docs.puppet.com/puppetserver/latest/tuning_guide.html
         server_jvm_max_heap_size => '256m',
@@ -57,7 +63,7 @@ describe 'Scenario: 2.6.0 to 2.7.2 upgrade:' do
         server_foreman        => false,
         server_reports        => 'store',
         server_external_nodes => '',
-        server_version        => '2.7.2',
+        server_version        => '#{to_version}',
         # only for install test - don't think to use this in production!
         # https://docs.puppet.com/puppetserver/latest/tuning_guide.html
         server_jvm_max_heap_size => '256m',
