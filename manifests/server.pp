d443141bbed9400bb50796a9a39a6e648250de16
Michael Moll <kvedulv@kvedulv.de>
2017-08-08 02:31:20 +0200

    use local variables for webserver.conf template

--- manifests/server.pp
+++ manifests/server.pp
@@ -426,15 +426,15 @@ class puppet::server(
   }
 
   if $ca {
-    $ssl_ca_cert = "${ssl_dir}/ca/ca_crt.pem"
-    $ssl_ca_crl  = "${ssl_dir}/ca/ca_crl.pem"
-    $ssl_chain   = $ssl_chain_filepath
-    $_crl_enable = pick($crl_enable, true)
+    $ssl_ca_cert     = "${ssl_dir}/ca/ca_crt.pem"
+    $ssl_ca_crl      = "${ssl_dir}/ca/ca_crl.pem"
+    $ssl_chain       = $ssl_chain_filepath
+    $crl_enable_real = pick($crl_enable, true)
   } else {
-    $ssl_ca_cert = "${ssl_dir}/certs/ca.pem"
-    $ssl_ca_crl  = pick($ca_crl_filepath, "${ssl_dir}/crl.pem")
-    $ssl_chain   = false
-    $_crl_enable = pick($crl_enable, false)
+    $ssl_ca_cert     = "${ssl_dir}/certs/ca.pem"
+    $ssl_ca_crl      = pick($ca_crl_filepath, "${ssl_dir}/crl.pem")
+    $ssl_chain       = false
+    $crl_enable_real = pick($crl_enable, false)
   }
 
   $ssl_cert      = "${ssl_dir}/certs/${certname}.pem"
