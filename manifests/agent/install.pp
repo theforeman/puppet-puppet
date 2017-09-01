7e2c116ba2e2c6636feeb593b84f3935c411cb24
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-09-01 22:34:49 +0200

    Use explicit parameters in puppet::agent::install
    On my machine this cuts down the test time from 24 seconds to 8 seconds.

--- manifests/agent/install.pp
+++ manifests/agent/install.pp
@@ -1,10 +1,16 @@
 # Install the puppet client installation
-class puppet::agent::install {
-  if $::puppet::manage_packages == true or $::puppet::manage_packages == 'agent' {
-    package { $::puppet::client_package:
-      ensure   => $::puppet::version,
-      provider => $::puppet::package_provider,
-      source   => $::puppet::package_source,
+class puppet::agent::install(
+  $manage_packages = $::puppet::manage_packages,
+  $package_name = $::puppet::client_package,
+  $package_version = $::puppet::version,
+  $package_provider = $::puppet::package_provider,
+  $package_source = $::puppet::package_source,
+) {
+  if $manage_packages == true or $manage_packages == 'agent' {
+    package { $package_name:
+      ensure   => $package_version,
+      provider => $package_provider,
+      source   => $package_source,
     }
   }
 }
--- spec/classes/puppet_agent_install_spec.rb
+++ spec/classes/puppet_agent_install_spec.rb
@@ -1,17 +1,72 @@
 require 'spec_helper'
 
 describe 'puppet::agent::install' do
+  context 'with explicit parameters' do
+    let :base_params do
+      {
+        :manage_packages  => true,
+        :package_name     => 'puppet-agent',
+        :package_version  => 'installed',
+        :package_provider => 'provider', # Can't set this to nil
+        :package_source   => 'source', # Can't set this to nil
+      }
+    end
+
+    describe 'base parameters' do
+      let :params do
+        base_params
+      end
+
+      it do
+        is_expected.to contain_package('puppet-agent').
+          with_ensure('installed').
+          with_provider('provider').
+          with_source('source')
+      end
+    end
+
+    describe 'when manage_packages => false' do
+      let :params do
+        base_params.merge(:manage_packages => false)
+      end
+
+      it { is_expected.not_to contain_package('puppet-agent') }
+    end
+
+    describe "when manage_packages => 'agent'" do
+      let :params do
+        base_params.merge(:manage_packages => 'agent')
+      end
+
+      it { is_expected.to contain_package('puppet-agent') }
+    end
+
+    describe "when manage_packages => 'server'" do
+      let :params do
+        base_params.merge(:manage_packages => 'sever')
+      end
+
+      it { is_expected.not_to contain_package('puppet-agent') }
+    end
+  end
+
   on_os_under_test.each do |os, facts|
     context "on #{os}" do
-      if facts[:osfamily] == 'FreeBSD'
-        if Puppet.version < '5.0'
-          client_package = 'puppet4'
-        else
-          client_package = 'puppet5'
-        end
-      else
-        client_package = 'puppet-agent'
-      end
+      client_package = if facts[:osfamily] == 'FreeBSD'
+                         if Puppet.version < '5.0'
+                           'puppet4'
+                         else
+                           'puppet5'
+                         end
+                       else
+                         'puppet-agent'
+                       end
+
+      package_provider = if facts[:osfamily] == 'windows'
+                           'chocolatey'
+                         else
+                           nil
+                         end
 
       let (:facts) do
         facts
@@ -22,78 +77,25 @@ describe 'puppet::agent::install' do
           'include ::puppet'
         end
 
-        if facts[:osfamily] == 'windows'
-          it 'should define provider as chocolatey on Windows' do
-            should contain_package(client_package).with_provider('chocolatey')
-          end
-        else
-          it 'should not define provider on non-Windows' do
-            should contain_package(client_package).without_provider(nil)
-          end
-        end
-      end
-
-      describe "when manage_packages => false" do
-        let :pre_condition do
-          "class { 'puppet': manage_packages => false }"
+        # For windows we specify a package provider which doesn't compile
+        if facts[:osfamily] != 'windows'
+          it { is_expected.to compile.with_all_deps }
         end
 
-        it 'should not contain Package[puppet]' do
-          should_not contain_package('puppet')
+        it do
+          is_expected.to contain_class('puppet::agent::install').
+            with_manage_packages(true).
+            with_package_name([client_package]).
+            with_package_version('present').
+            with_package_provider(package_provider).
+            with_package_source(nil)
         end
 
-        it 'should not contain Package[puppet-agent]' do
-          should_not contain_package('puppet-agent')
-        end
-      end
-
-      describe "when manage_packages => 'agent'" do
-        let :pre_condition do
-          "class { 'puppet': manage_packages => 'agent' }"
-        end
-
-        it 'should contain Package[puppet]' do
-          should contain_package(client_package)
-        end
-      end
-
-      describe "when manage_packages => 'server'" do
-        let :pre_condition do
-          "class { 'puppet': manage_packages => 'server' }"
-        end
-
-        it 'should not contain Package[puppet]' do
-          should_not contain_package('puppet')
-        end
-
-        it 'should not contain Package[puppet-agent]' do
-          should_not contain_package('puppet-agent')
-        end
-      end
-
-      if facts[:osfamily] == 'windows'
-        describe "when package_provider => 'msi'" do
-          let :pre_condition do
-            "class { 'puppet': package_provider => 'msi', }"
-          end
-
-          it 'should define provider as msi' do
-            should contain_package(client_package).with_provider('msi')
-          end
-        end
-
-        describe "when package_provider => 'windows' and source is defined" do
-          let :pre_condition do
-            "class { 'puppet': package_provider => 'windows', package_source => 'C:\\Temp\\puppet.exe' }"
-          end
-
-          it 'should define provider as windows' do
-            should contain_package(client_package).with_provider('windows')
-          end
-
-          it 'should define source as C:\Temp\puppet.exe' do
-            should contain_package(client_package).with_source('C:\Temp\puppet.exe')
-          end
+        it do
+          is_expected.to contain_package(client_package).
+            with_ensure('present').
+            with_provider(package_provider).
+            with_source(nil)
         end
       end
     end
