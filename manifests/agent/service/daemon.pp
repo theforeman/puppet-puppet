79a1739c7c4ef64ba4156f888e3c75cbcf0b63e9
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-07-23 11:35:08 +0200

    Simplify puppet::agent::daemon
    With Puppet 4 data types we can simplify the complicated case statement
    to a if/else.

--- manifests/agent/service/daemon.pp
+++ manifests/agent/service/daemon.pp
@@ -1,27 +1,23 @@
 class puppet::agent::service::daemon (
-  $enabled = false,
+  Boolean $enabled = false,
 ) {
-  if ! ('service' in $::puppet::unavailable_runmodes) {
-    case $enabled {
-      true: {
-        service {'puppet':
-          ensure     => running,
-          name       => $puppet::service_name,
-          hasstatus  => true,
-          hasrestart => $puppet::agent_restart_command != undef,
-          enable     => true,
-          restart    => $puppet::agent_restart_command,
-        }
+  unless 'service' in $::puppet::unavailable_runmodes {
+    if $enabled {
+      service {'puppet':
+        ensure     => running,
+        name       => $puppet::service_name,
+        hasstatus  => true,
+        hasrestart => $puppet::agent_restart_command != undef,
+        enable     => true,
+        restart    => $puppet::agent_restart_command,
       }
-      false: {
-        service {'puppet':
-          ensure    => stopped,
-          name      => $puppet::service_name,
-          hasstatus => true,
-          enable    => false,
-        }
+    } else {
+      service {'puppet':
+        ensure    => stopped,
+        name      => $puppet::service_name,
+        hasstatus => true,
+        enable    => false,
       }
-      default: { fail('puppet::agent::service::daemon::enabled should be true or false') }
     }
   }
 }
