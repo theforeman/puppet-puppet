9799e23d441c75f2a13b8903e2eb35b470592200
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-10-26 12:19:42 +0200

    Unify all agent specs in a single file
    With rspec-puppet you're not supposed to test individual classes that
    should be private. This also makes it more efficient because we had
    quite a few duplicated cases.

--- spec/classes/puppet_agent_spec.rb
+++ spec/classes/puppet_agent_spec.rb
@@ -1,79 +1,347 @@
 require 'spec_helper'
+require 'deep_merge'
 
-describe 'puppet::agent' do
+describe 'puppet' do
   on_os_under_test.each do |os, facts|
     context "on #{os}" do
       case facts[:osfamily]
       when 'FreeBSD'
-        if Puppet.version < '5.0'
-          client_package = 'puppet4'
-        else
-          client_package = 'puppet5'
-        end
-        confdir          = '/usr/local/etc/puppet'
+        bindir = '/usr/local/bin'
+        client_package = Puppet.version < '5.0' ? 'puppet4' : 'puppet5'
+        confdir = '/usr/local/etc/puppet'
+        package_provider = nil
       when 'windows'
+        bindir = 'C:/ProgramData/PuppetLabs/puppet/bin'
         client_package = 'puppet-agent'
-        confdir        = 'C:/ProgramData/PuppetLabs/puppet/etc'
+        confdir = 'C:/ProgramData/PuppetLabs/puppet/etc'
+        package_provider = 'chocolatey'
       else
+        bindir = '/opt/puppetlabs/bin'
         client_package = 'puppet-agent'
-        confdir        = '/etc/puppetlabs/puppet'
+        confdir = '/etc/puppetlabs/puppet'
+        package_provider = nil
       end
 
       let :facts do
-        facts
+        facts.deep_merge(
+          # rspec-puppet(-facts) doesn't mock this
+          clientcert: 'client.example.com',
+          # Cron/systemd timers are based on the IP - make it consistent
+          networking: { ip: '192.0.2.100' }
+        )
+      end
+
+      let :params do
+        {
+          agent: true
+        }
       end
 
       describe 'with no custom parameters' do
-        let :pre_condition do
-          "class {'puppet': agent => true}"
+        # For windows we specify a package provider which doesn't compile
+        if facts[:osfamily] != 'windows'
+          it { is_expected.to compile.with_all_deps }
         end
-        it { should contain_class('puppet::agent::install') }
-        it { should contain_class('puppet::agent::config') }
-        it { should contain_class('puppet::agent::service') }
-        it { should contain_file(confdir).with_ensure('directory') }
-        it { should contain_concat("#{confdir}/puppet.conf") }
-        it { should contain_package(client_package).with_ensure('present') }
+
+        # install
         it do
-          should contain_concat__fragment('puppet.conf_agent').
-            with_content(/^\[agent\]/).
-            with({})
+          is_expected.to contain_class('puppet::agent::install')
+            .with_manage_packages(true)
+            .with_package_name([client_package])
+            .with_package_version('present')
+            .with_package_provider(package_provider)
+            .with_package_source(nil)
+            .that_notifies(['Class[puppet::agent::config]', 'Class[puppet::agent::service]'])
         end
 
         it do
-          should_not contain_puppet__config__agent('prerun_command')
+          is_expected.to contain_package(client_package)
+            .with_ensure('present')
+            .with_provider(package_provider)
+            .with_source(nil)
         end
 
+        # config
+        it { is_expected.to contain_class('puppet::agent::config').that_notifies('Class[puppet::agent::service]') }
+        it { is_expected.to contain_file(confdir).with_ensure('directory') }
+        it { is_expected.to contain_concat("#{confdir}/puppet.conf") }
+        it { is_expected.to contain_concat__fragment('puppet.conf_agent').with_content(/^\[agent\]/) }
+        it { is_expected.to contain_puppet__config__agent('certname').with_value(facts[:clientcert]) }
+        it { is_expected.to contain_puppet__config__agent('report').with_value('true') }
+        it { is_expected.not_to contain_puppet__config__agent('prerun_command') }
+        it { is_expected.not_to contain_puppet__config__agent('postrun_command') }
+
+        if facts[:osfamily] == 'Debian'
+          it do
+            is_expected.to contain_augeas('puppet::set_start')
+              .with_context('/files/etc/default/puppet')
+              .with_changes('set START yes')
+              .with_incl('/etc/default/puppet')
+              .with_lens('Shellvars.lns')
+          end
+          it { is_expected.to contain_file('/var/lib/puppet/state/agent_disabled.lock').with_ensure(:absent) }
+        end
+
+        # service
+        it { is_expected.to contain_class('puppet::agent::service') }
+
+        it { is_expected.to contain_class('puppet::agent::service::daemon').with_enabled(true) }
         it do
-          should_not contain_puppet__config__agent('postrun_command')
+          is_expected.to contain_service('puppet')
+            .with_ensure('running')
+            .with_name('puppet')
+            .with_hasstatus('true')
+            .with_enable('true')
+        end
+
+        it { is_expected.to contain_class('puppet::agent::service::cron').with_enabled(false) }
+        if os =~ /\A(windows|archlinux)/
+          it { is_expected.not_to contain_cron('puppet') }
+        else
+          it { is_expected.to contain_cron('puppet').with_ensure('absent') }
+        end
+
+        it { is_expected.to contain_class('puppet::agent::service::systemd').with_enabled(false) }
+        case os
+        when /\Adebian-/, /\A(redhat|centos|scientific)-7/, /\Afedora-/, /\Aubuntu-(16|18)/, /\Aarchlinux-/
+          it do
+            is_expected.to contain_exec('systemctl-daemon-reload-puppet')
+              .with_refreshonly(true)
+              .with_command('systemctl daemon-reload')
+          end
+
+          it do
+            is_expected.to contain_service('puppet-run.timer')
+              .with_ensure(:stopped)
+              .with_provider('systemd')
+              .with_name('puppet-run.timer')
+              .with_enable(false)
+          end
+
+          it { is_expected.to contain_file('/etc/systemd/system/puppet-run.timer').with_ensure(:absent) }
+          it { is_expected.to contain_file('/etc/systemd/system/puppet-run.service').with_ensure(:absent) }
+        else
+          it { is_expected.not_to contain_service('puppet-run.timer') }
+          it { is_expected.not_to contain_file('/etc/systemd/system/puppet-run.timer') }
+          it { is_expected.not_to contain_file('/etc/systemd/system/puppet-run.service') }
+          it { is_expected.not_to contain_exec('systemctl-daemon-reload-puppet') }
         end
       end
 
       describe 'set prerun_command will be included in config' do
-        let(:pre_condition) { "class {'puppet': agent => true, prerun_command => '/my/prerun'}" }
-        it do
-          should contain_puppet__config__agent('prerun_command').with({'value'  => '/my/prerun'})
+        let :params do
+          super().merge(prerun_command: '/my/prerun')
         end
+
+        it { is_expected.to contain_puppet__config__agent('prerun_command').with_value('/my/prerun') }
       end
 
       describe 'set postrun_command will be included in config' do
-        let(:pre_condition) { "class {'puppet': agent => true, postrun_command => '/my/postrun'}" }
-        it do
-          should contain_puppet__config__agent('postrun_command').with({'value'  => '/my/postrun'})
+        let :params do
+          super().merge(postrun_command: '/my/postrun')
         end
+
+        it { is_expected.to contain_puppet__config__agent('postrun_command').with_value('/my/postrun') }
       end
 
       describe 'with additional settings' do
-        let :pre_condition do
-          "class {'puppet':
-              agent_additional_settings => {ignoreschedules => true},
-           }"
+        let :params do
+          super().merge(agent_additional_settings: { 'ignoreschedules' => true })
         end
 
-        it 'should configure puppet.conf' do
-          should contain_puppet__config__agent('ignoreschedules').with({'value'  => 'true'})
+        it { is_expected.to contain_puppet__config__agent('ignoreschedules').with_value('true') }
+      end
+
+      context 'manage_packages' do
+        describe 'when manage_packages => false' do
+          let :params do
+            super().merge(manage_packages: false)
+          end
+
+          it { is_expected.not_to contain_package('puppet-agent') }
+        end
+
+        describe "when manage_packages => 'agent'" do
+          let :params do
+            super().merge(manage_packages: 'agent')
+          end
+
+          it { is_expected.to contain_package('puppet-agent') }
+        end
+
+        describe "when manage_packages => 'server'" do
+          let :params do
+            super().merge(manage_packages: 'server')
+          end
+
+          it { is_expected.not_to contain_package('puppet-agent') }
         end
       end
 
+      context 'runmode' do
+        describe 'when runmode => cron' do
+          let :params do
+            super().merge(runmode: 'cron')
+          end
+
+          case os
+          when /\A(windows|archlinux)/
+            it { is_expected.to raise_error(Puppet::Error, /Runmode of cron not supported on #{facts[:kernel]} operating systems!/) }
+          when /\Adebian-/, /\A(redhat|centos|scientific)-7/, /\Afedora-/, /\Aubuntu-(16|18)/
+            it { is_expected.to compile.with_all_deps }
+            it { is_expected.to contain_concat__fragment('puppet.conf_agent') }
+            if facts[:osfamily] == 'Debian'
+              it do
+                is_expected.to contain_augeas('puppet::set_start')
+                  .with_context('/files/etc/default/puppet')
+                  .with_changes('set START no')
+                  .with_incl('/etc/default/puppet')
+                  .with_lens('Shellvars.lns')
+              end
+              it { is_expected.to contain_file('/var/lib/puppet/state/agent_disabled.lock').with_ensure(:absent) }
+            end
+
+            it { is_expected.to contain_class('puppet::agent::service::cron').with_enabled(true) }
+            it { is_expected.to contain_class('puppet::agent::service::daemon').with_enabled(false) }
+            it do
+              is_expected.to contain_service('puppet')
+                .with_ensure('stopped')
+                .with_name('puppet')
+                .with_hasstatus('true')
+                .with_enable('false')
+            end
+            it { is_expected.to contain_class('puppet::agent::service::systemd').with_enabled(false) }
+            it { is_expected.to contain_service('puppet-run.timer').with_ensure(:stopped) }
+            it do
+              is_expected.to contain_cron('puppet')
+                .with_command("#{bindir}/puppet agent --config #{confdir}/puppet.conf --onetime --no-daemonize")
+                .with_user('root')
+                .with_minute(%w[10 40])
+                .with_hour('*')
+            end
+          else
+            it { is_expected.to compile.with_all_deps }
+            it { is_expected.to contain_class('puppet::agent::service::cron').with_enabled(true) }
+            it { is_expected.to contain_class('puppet::agent::service::daemon').with_enabled(false) }
+            it { is_expected.to contain_class('puppet::agent::service::systemd').with_enabled(false) }
+            it { is_expected.not_to contain_service('puppet-run.timer') }
+            it do
+              is_expected.to contain_cron('puppet')
+                .with_command("#{bindir}/puppet agent --config #{confdir}/puppet.conf --onetime --no-daemonize")
+                .with_user('root')
+                .with_minute(%w[10 40])
+                .with_hour('*')
+            end
+          end
+        end
+
+        describe 'when runmode => systemd.timer' do
+          let :params do
+            super().merge(runmode: 'systemd.timer')
+          end
+
+          case os
+          when /\Adebian-/, /\A(redhat|centos|scientific)-7/, /\Afedora-/, /\Aubuntu-(16|18)/, /\Aarchlinux-/
+            it { is_expected.to compile.with_all_deps }
+            it { is_expected.to contain_class('puppet::agent::service::daemon').with_enabled(false) }
+            it { is_expected.to contain_class('puppet::agent::service::cron').with_enabled(false) }
+            it { is_expected.to contain_class('puppet::agent::service::systemd').with_enabled(true) }
+            it { is_expected.to contain_service('puppet-run.timer').with_ensure(:running) }
+
+            it do
+              is_expected.to contain_file('/etc/systemd/system/puppet-run.timer')
+                .with_content(/.*OnCalendar\=\*-\*-\* \*\:10,40:00.*/)
+            end
+
+            it do
+              is_expected.to contain_file('/etc/systemd/system/puppet-run.timer')
+                .with_content(/^RandomizedDelaySec\=0$/)
+            end
+
+            it do
+              is_expected.to contain_file('/etc/systemd/system/puppet-run.service')
+                .with_content(%r{.*ExecStart=#{bindir}/puppet agent --config #{confdir}/puppet.conf --onetime --no-daemonize.*})
+            end
+
+            it do
+              is_expected.to contain_exec('systemctl-daemon-reload-puppet')
+                .with_refreshonly(true)
+                .with_command('systemctl daemon-reload')
+            end
+
+            it do
+              is_expected.to contain_service('puppet-run.timer')
+                .with_provider('systemd')
+                .with_ensure('running')
+                .with_name('puppet-run.timer')
+                .with_enable('true')
+            end
+          else
+            it { is_expected.to raise_error(Puppet::Error, /Runmode of systemd.timer not supported on #{facts[:kernel]} operating systems!/) }
+          end
+        end
+
+        describe 'when runmode => none' do
+          let :params do
+            super().merge(runmode: 'none')
+          end
+
+          # For windows we specify a package provider which doesn't compile
+          if facts[:osfamily] != 'windows'
+            it { is_expected.to compile.with_all_deps }
+          end
+          it { is_expected.to contain_class('puppet::agent::service::daemon').with_enabled(false) }
+          it { is_expected.to contain_class('puppet::agent::service::cron').with_enabled(false) }
+          it { is_expected.to contain_class('puppet::agent::service::systemd').with_enabled(false) }
+
+          case os
+          when /\Adebian-/, /\A(redhat|centos|scientific)-7/, /\Afedora-/, /\Aubuntu-(16|18)/, /\Aarchlinux-/
+            it { is_expected.to contain_service('puppet-run.timer').with_ensure(:stopped) }
+          else
+            it { is_expected.not_to contain_service('puppet-run.timer') }
+          end
+        end
+      end
+
+      describe 'when unavailable_runmodes => ["cron"]' do
+        let :params do
+          super().merge(unavailable_runmodes: ['cron'])
+        end
+
+        it { is_expected.not_to contain_cron('puppet') }
+      end
+
+      describe 'with custom service_name' do
+        let :params do
+          super().merge(service_name: 'pe-puppet')
+        end
+
+        it { is_expected.to contain_service('puppet').with_name('pe-puppet') }
+      end
+
+      context 'with remove_lock => false' do
+        let :params do
+          super().merge(remove_lock: false)
+        end
+
+        it { should_not contain_file('/var/lib/puppet/state/agent_disabled.lock') }
+      end
+
+      context 'with client_certname => false' do
+        let :params do
+          super().merge(client_certname: false)
+        end
+
+        it { is_expected.not_to contain_puppet__config__agent('certname') }
+      end
+
+      context 'with report => false' do
+        let :params do
+          super().merge(report: false)
+        end
+
+        it { is_expected.to contain_puppet__config__agent('report').with_value('false') }
+      end
     end
   end
 end
