f203124fc8c06c6e42d9d3ee2d699729f6c6286f
Michael Moll <kvedulv@kvedulv.de>
2018-12-26 00:40:07 +0100

    Install JDK 8 on Debian/jessie for Puppetserver 5+

--- spec/acceptance/puppetserver_latest_spec.rb
+++ spec/acceptance/puppetserver_latest_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper_acceptance'
 
 describe 'Scenario: install puppetserver (latest):' do
   before(:context) do
+    if fact('lsbdistcodename') == 'jessie' && ENV['BEAKER_PUPPET_COLLECTION'] != 'pc1'
+      on default, "echo 'deb http://deb.debian.org/debian jessie-backports main' >/etc/apt/sources.list.d/backports.list"
+      on default, 'apt update'
+      on default, 'apt -y -t jessie-backports install openjdk-8-jdk-headless'
+    end
     if check_for_package(default, 'puppetserver')
       on default, puppet('resource package puppetserver ensure=purged')
       on default, 'rm -rf /etc/sysconfig/puppetserver /etc/puppetlabs/puppetserver'
