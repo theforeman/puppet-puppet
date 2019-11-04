201315cccaf4fcb4d9ddea9cdf45655387bd8b3f
Frank Wall <fw@moov.de>
2019-11-04 17:59:19 +0100

    restart Puppet Agent service after updating the package

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -5,6 +5,6 @@ class puppet::agent {
   contain puppet::agent::config
   contain puppet::agent::service
 
-  Class['puppet::agent::install'] ~> Class['puppet::agent::config']
+  Class['puppet::agent::install'] ~> Class['puppet::agent::config', 'puppet::agent::service']
   Class['puppet::config', 'puppet::agent::config'] ~> Class['puppet::agent::service']
 }
