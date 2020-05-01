72ef3f78857e4a1d7b3dbf10ad665b6255c3c06e
Daniel S. Reichenbach <daniel@kogito.network>
2020-05-01 12:30:48 +0200

    Support Ubuntu Focal

--- spec/support/acceptance/puppetserver.rb
+++ spec/support/acceptance/puppetserver.rb
@@ -1,3 +1,4 @@
 def unsupported_puppetserver
-  host_inventory['facter']['os']['name'] == 'Fedora'
+  host_inventory['facter']['os']['name'] == 'Fedora' ||
+    (host_inventory['facter']['os']['name'] == 'Ubuntu' && host_inventory['facter']['os']['distro']['codename'] == 'focal')
 end
