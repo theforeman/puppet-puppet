e799269abbe053933dadfdfc28db22cf80e86eda
Tim Meusel <tim@bastelfreak.de>
2021-03-24 09:32:35 +0100

    (#780) puppetserver: Disable http-client metrics
    This parameter only enables/disables the *http-client* metrics within
    puppetserver. It's disabled by default in the Puppet Inc. config file.

--- manifests/init.pp
+++ manifests/init.pp
@@ -437,8 +437,8 @@
 #
 # $server_puppetserver_jruby9k::            For Puppetserver 5, use JRuby 9k? Defaults to false
 #
-# $server_puppetserver_metrics::            Enable metrics (Puppetserver 5.x only)
-#                                           Defaults to true on Puppetserver 5.x and to false on Puppetserver 2.x
+# $server_puppetserver_metrics::            Enable puppetserver http-client metrics
+#                                           Defaults to false because that's the Puppet Inc. default behaviour.
 #
 # $server_puppetserver_profiler::           Enable JRuby profiling.
 #                                           Defaults to false because that's the Puppet Inc. default behaviour.
@@ -709,7 +709,7 @@ class puppet (
   Boolean $server_allow_header_cert_info = $puppet::params::server_allow_header_cert_info,
   Integer[0] $server_web_idle_timeout = $puppet::params::server_web_idle_timeout,
   Boolean $server_puppetserver_jruby9k = $puppet::params::server_puppetserver_jruby9k,
-  Optional[Boolean] $server_puppetserver_metrics = $puppet::params::server_puppetserver_metrics,
+  Boolean $server_puppetserver_metrics = false,
   Boolean $server_puppetserver_profiler = false,
   Boolean $server_metrics_jmx_enable = $puppet::params::server_metrics_jmx_enable,
   Boolean $server_metrics_graphite_enable = $puppet::params::server_metrics_graphite_enable,
--- manifests/server/puppetserver.pp
+++ manifests/server/puppetserver.pp
@@ -111,7 +111,7 @@ class puppet::server::puppetserver (
   $server_check_for_updates               = $puppet::server::check_for_updates,
   $server_environment_class_cache_enabled = $puppet::server::environment_class_cache_enabled,
   $server_jruby9k                         = $puppet::server::puppetserver_jruby9k,
-  $server_metrics                         = $puppet::server::real_puppetserver_metrics,
+  $server_metrics                         = $puppet::server::puppetserver_metrics,
   $server_profiler                        = $puppet::server::puppetserver_profiler,
   $metrics_jmx_enable                     = $puppet::server::metrics_jmx_enable,
   $metrics_graphite_enable                = $puppet::server::metrics_graphite_enable,
