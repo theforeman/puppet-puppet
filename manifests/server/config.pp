bdbc335e67b61f8f7561bd54024072e2a0a8a2db
Peter Ong <peter@poproj.com>
2021-04-11 20:59:31 -0700

    Change to address @ekohl's suggestion.

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -157,14 +157,15 @@ class puppet::server::config inherits puppet::config {
 
   # Generate a new CA and host cert if our host cert doesn't exist
   if $puppet::server::ca {
-    if versioncmp($::puppetversion, '6.0') > 0 {
-      $creates = $puppet::server::ssl_ca_cert
-      $command = "${puppet::puppetserver_cmd} ca setup"
-    } else {
-      $creates = $puppet::server::ssl_cert
-      $command = "${puppet::puppet_cmd} cert --generate ${puppet::server::certname} --allow-dns-alt-names"
-    }
     if $puppet::generate_ca_cert {
+      if versioncmp($::puppetversion, '6.0') > 0 {
+        $creates = $puppet::server::ssl_ca_cert
+        $command = "${puppet::puppetserver_cmd} ca setup"
+      } else {
+        $creates = $puppet::server::ssl_cert
+        $command = "${puppet::puppet_cmd} cert --generate ${puppet::server::certname} --allow-dns-alt-names"
+      }
+
       exec {'puppet_server_config-generate_ca_cert':
         creates => $creates,
         command => $command,
