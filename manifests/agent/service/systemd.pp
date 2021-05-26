0b8653ca219fe839033331d3a5a8a480f08db0dc
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-05-26 11:07:27 +0200

    Revert "Revert "Convert the systemd timer service to camptocamp/systemd""
    This reverts commit 904e51b3e0b2fc8b2a3098ca981ad3e4339dd611.
    
    Initially it was reverted since it led to a dependency cycle in the
    Foreman installer. This was caused by a systemctl daemon-reload. Since
    this was a bit of an edge case and not all users will hit this, it's
    reinstated.
    
    The cyclic dependency can also be mitigated by using camptocamp/systemd
    3.x which no longer performs the exec. This only works on Puppet 6.1 or
    newer though.

--- manifests/agent/service/systemd.pp
+++ manifests/agent/service/systemd.pp
@@ -6,69 +6,22 @@ class puppet::agent::service::systemd (
   Optional[Integer[0,59]] $minute  = undef,
 ) {
   unless $puppet::runmode == 'unmanaged' or 'systemd.timer' in $puppet::unavailable_runmodes {
-    exec { 'systemctl-daemon-reload-puppet':
-      refreshonly => true,
-      path        => $::path,
-      command     => 'systemctl daemon-reload',
-    }
+    # Use the same times as for cron
+    $times = extlib::ip_to_cron($puppet::runinterval)
 
-    if $enabled {
-      # Use the same times as for cron
-      $times = extlib::ip_to_cron($puppet::runinterval)
+    # But only if they are not explicitly specified
+    $_hour = pick($hour, $times[0])
+    $_minute = pick($minute, $times[1])
 
-      # But only if they are not explicitly specified
-      $_hour = pick($hour, $times[0])
-      $_minute = pick($minute, $times[1])
+    $command = pick($puppet::systemd_cmd, "${puppet::puppet_cmd} agent --config ${puppet::dir}/puppet.conf --onetime --no-daemonize --detailed-exitcode --no-usecacheonfailure")
+    $randomizeddelaysec = $puppet::systemd_randomizeddelaysec
 
-      $command = $puppet::systemd_cmd ? {
-        undef   => "${puppet::puppet_cmd} agent --config ${puppet::dir}/puppet.conf --onetime --no-daemonize --detailed-exitcode --no-usecacheonfailure",
-        default => $puppet::systemd_cmd,
-      }
-
-      $randomizeddelaysec = $puppet::systemd_randomizeddelaysec
-
-      file { "/etc/systemd/system/${puppet::systemd_unit_name}.timer":
-        content => template('puppet/agent/systemd.puppet-run.timer.erb'),
-        notify  => [
-          Exec['systemctl-daemon-reload-puppet'],
-          Service['puppet-run.timer'],
-        ],
-      }
-
-      file { "/etc/systemd/system/${puppet::systemd_unit_name}.service":
-        content => template('puppet/agent/systemd.puppet-run.service.erb'),
-        notify  => Exec['systemctl-daemon-reload-puppet'],
-      }
-
-      service { 'puppet-run.timer':
-        ensure   => running,
-        provider => 'systemd',
-        name     => "${puppet::systemd_unit_name}.timer",
-        enable   => true,
-        require  => Exec['systemctl-daemon-reload-puppet'],
-      }
-    } else {
-      # Reverse order - stop, delete files, exec
-      service { 'puppet-run.timer':
-        ensure   => stopped,
-        provider => 'systemd',
-        name     => "${puppet::systemd_unit_name}.timer",
-        enable   => false,
-        before   => [
-          File["/etc/systemd/system/${puppet::systemd_unit_name}.timer"],
-          File["/etc/systemd/system/${puppet::systemd_unit_name}.service"],
-        ],
-      }
-
-      file { "/etc/systemd/system/${puppet::systemd_unit_name}.timer":
-        ensure => absent,
-        notify => Exec['systemctl-daemon-reload-puppet'],
-      }
-
-      file { "/etc/systemd/system/${puppet::systemd_unit_name}.service":
-        ensure => absent,
-        notify => Exec['systemctl-daemon-reload-puppet'],
-      }
+    systemd::timer { "${puppet::systemd_unit_name}.timer":
+      ensure          => bool2str($enabled, 'present', 'absent'),
+      active          => $enabled,
+      enable          => $enabled,
+      timer_content   => template('puppet/agent/systemd.puppet-run.timer.erb'),
+      service_content => template('puppet/agent/systemd.puppet-run.service.erb'),
     }
   }
 }
