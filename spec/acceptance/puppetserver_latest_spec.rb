28db556bc7c4f1d11a1d6798ace87f98a6438dcd
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-06-04 15:12:53 +0200

    Combine acceptance test files
    Config is testing the same thing latest and there's no need to clean the
    entire puppetserver in between. This speeds up the execution time.
    
    The upgrade tests are combined into a single file and also includes
    Puppet 7 upgrade tests now.

--- spec/acceptance/puppetserver_latest_spec.rb
+++ spec/acceptance/puppetserver_latest_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper_acceptance'
 
 describe 'Scenario: install puppetserver (latest):', unless: unsupported_puppetserver do
-  before(:context) do
+  before(:all) do
     if check_for_package(default, 'puppetserver')
       on default, puppet('resource package puppetserver ensure=purged')
       on default, 'rm -rf /etc/sysconfig/puppetserver /etc/puppetlabs/puppetserver'
@@ -13,13 +13,44 @@ describe 'Scenario: install puppetserver (latest):', unless: unsupported_puppets
     raise 'At least 2048MB free memory required' if memoryfree_mb < 256
   end
 
-  it_behaves_like 'an idempotent resource' do
-    let(:manifest) do
-      <<-EOS
-      class { 'puppet':
-        server => true,
-      }
-      EOS
+  context 'default options' do
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-EOS
+        class { 'puppet':
+          server => true,
+        }
+        EOS
+      end
+    end
+  end
+
+  describe 'server_max_open_files' do
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-MANIFEST
+        class { 'puppet':
+          server                => true,
+          server_max_open_files => 32143,
+        }
+
+        # Puppet 5 + puppet/systemd 3 workaround
+        if $puppet::server_max_open_files and versioncmp($facts['puppetversion'], '6.1') < 0 {
+          exec { 'puppetserver-systemctl-daemon-reload':
+            command     => 'systemctl daemon-reload',
+            refreshonly => true,
+            path        => $facts['path'],
+            subscribe   => File['/etc/systemd/system/puppetserver.service.d/limits.conf'],
+          }
+        }
+        MANIFEST
+      end
+    end
+
+    # pgrep -f java.*puppetserver would be better. But i cannot get it to work. Shellwords.escape() seems to break something
+    describe command("grep '^Max open files' /proc/`cat /var/run/puppetlabs/puppetserver/puppetserver.pid`/limits"), :sudo => true do
+      its(:exit_status) { is_expected.to eq 0 }
+      its(:stdout) { is_expected.to match %r{^Max open files\s+32143\s+32143\s+files\s*$} }
     end
   end
 end
--- spec/acceptance/puppetserver_upgrade_spec.rb
+++ spec/acceptance/puppetserver_upgrade_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper_acceptance'
 
-describe 'Scenario: 5.3.6 to 5.3.7 upgrade:', if: ENV['BEAKER_PUPPET_COLLECTION'] == 'puppet5', unless: unsupported_puppetserver do
-  before(:context) do
+describe 'Scenario: minor version upgrade', unless: unsupported_puppetserver do
+  before(:all) do
     if check_for_package(default, 'puppetserver')
       on default, puppet('resource package puppetserver ensure=purged')
       on default, 'rm -rf /etc/sysconfig/puppetserver /etc/puppetlabs/puppetserver'
@@ -13,29 +13,43 @@ describe 'Scenario: 5.3.6 to 5.3.7 upgrade:', if: ENV['BEAKER_PUPPET_COLLECTION'
     raise 'At least 256MB free memory required' if memoryfree_mb < 256
   end
 
-  case fact('osfamily')
-  when 'Debian'
-    from_version = "5.3.6-1#{fact('lsbdistcodename')}"
-    to_version = "5.3.7-1#{fact('lsbdistcodename')}"
-  else
+  case ENV['BEAKER_PUPPET_COLLECTION']
+  when 'puppet7'
+    from_version = '7.0.0'
+    to_version = '7.2.0'
+  when 'puppet6'
+    from_version = '6.7.0'
+    to_version = '6.7.2'
+  when 'puppet5'
     from_version = '5.3.6'
     to_version = '5.3.7'
+  else
+    raise 'Unsupported Puppet collection'
   end
 
-  context 'install 5.3.6' do
+  case fact('osfamily')
+  when 'Debian'
+    from_version_exact = "#{from_version}-1#{fact('lsbdistcodename')}"
+    to_version_exact = "#{to_version}-1#{fact('lsbdistcodename')}"
+  else
+    from_version_exact = from_version
+    to_version_exact = to_version
+  end
+
+  context "install #{from_version}" do
     it_behaves_like 'an idempotent resource' do
       let(:manifest) do
         <<-EOS
         class { 'puppet':
           server         => true,
-          server_version => '#{from_version}',
+          server_version => '#{from_version_exact}',
         }
         EOS
       end
     end
 
     describe command('puppetserver --version') do
-      its(:stdout) { is_expected.to match("puppetserver version: 5.3.6\n") }
+      its(:stdout) { is_expected.to match("puppetserver version: #{from_version}\n") }
     end
 
     describe service('puppetserver') do
@@ -48,20 +62,20 @@ describe 'Scenario: 5.3.6 to 5.3.7 upgrade:', if: ENV['BEAKER_PUPPET_COLLECTION'
     end
   end
 
-  context 'upgrade to 5.3.7' do
+  context "upgrade to #{to_version}" do
     it_behaves_like 'an idempotent resource' do
       let(:manifest) do
         <<-EOS
         class { 'puppet':
           server         => true,
-          server_version => '#{to_version}',
+          server_version => '#{to_version_exact}',
         }
         EOS
       end
     end
 
     describe command('puppetserver --version') do
-      its(:stdout) { is_expected.to match("puppetserver version: 5.3.7\n") }
+      its(:stdout) { is_expected.to match("puppetserver version: #{to_version}\n") }
     end
 
     describe service('puppetserver') do
