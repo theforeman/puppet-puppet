a81f0e532488f242fe3d5e87b21f5a9e8915ec72
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-09-01 17:47:45 +0200

    Refs #35985 - Implement optional Katello integration
    Katello uses a different certificate structure. This moves over the
    integration bits from puppet-foreman_proxy_content to this module. It
    also means fewer variables need to be set in the installer itself.

--- manifests/server/foreman.pp
+++ manifests/server/foreman.pp
@@ -1,6 +1,21 @@
 # @summary Set up Foreman integration
 # @api private
-class puppet::server::foreman {
+class puppet::server::foreman (
+  Boolean $katello = false,
+) {
+  if $katello {
+    include certs::puppet
+    Class['certs::puppet'] -> Class['puppetserver_foreman']
+
+    $ssl_ca = $certs::puppet::ssl_ca_cert
+    $ssl_cert = $certs::puppet::client_cert
+    $ssl_key = $certs::puppet::client_key
+  } else {
+    $ssl_ca = pick($puppet::server::foreman_ssl_ca, $puppet::server::ssl_ca_cert)
+    $ssl_cert = pick($puppet::server::foreman_ssl_cert, $puppet::server::ssl_cert)
+    $ssl_key = pick($puppet::server::foreman_ssl_key, $puppet::server::ssl_cert_key)
+  }
+
   # Include foreman components for the puppetmaster
   # ENC script, reporting script etc.
   class { 'puppetserver_foreman':
@@ -10,9 +25,9 @@ class puppet::server::foreman {
     puppet_home      => $puppet::server::puppetserver_vardir,
     puppet_basedir   => $puppet::server::puppet_basedir,
     puppet_etcdir    => $puppet::dir,
-    ssl_ca           => pick($puppet::server::foreman_ssl_ca, $puppet::server::ssl_ca_cert),
-    ssl_cert         => pick($puppet::server::foreman_ssl_cert, $puppet::server::ssl_cert),
-    ssl_key          => pick($puppet::server::foreman_ssl_key, $puppet::server::ssl_cert_key),
+    ssl_ca           => $ssl_ca,
+    ssl_cert         => $ssl_cert,
+    ssl_key          => $ssl_key,
   }
   contain puppetserver_foreman
 }
