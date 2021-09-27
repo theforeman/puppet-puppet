cf3a707d31608df660686f58bc3aa692a81e3b53
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-09-27 16:33:53 +0200

    Remove Puppet start workaround on Debian
    Back with Puppet 3 there was a workaround to not start Puppet by default
    but since Debian Jessie ([854487]) this no longer exists. It now uses
    regular init integration. This also doesn't affect AIO which never
    implemented it this way.
    
    [854487]: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=854487

--- manifests/agent/config.pp
+++ manifests/agent/config.pp
@@ -48,24 +48,4 @@ class puppet::agent::config inherits puppet::config {
   $puppet::agent_additional_settings.each |$key,$value| {
     puppet::config::agent { $key: value => $value }
   }
-
-  if $puppet::runmode == 'service' {
-    $should_start = 'yes'
-  } else {
-    $should_start = 'no'
-  }
-
-  if $facts['os']['family'] == 'Debian' {
-    augeas {'puppet::set_start':
-      context => '/files/etc/default/puppet',
-      changes => "set START ${should_start}",
-      incl    => '/etc/default/puppet',
-      lens    => 'Shellvars.lns',
-    }
-    if $puppet::remove_lock {
-      file {'/var/lib/puppet/state/agent_disabled.lock':
-        ensure => absent,
-      }
-    }
-  }
 }
