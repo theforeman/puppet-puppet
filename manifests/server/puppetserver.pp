theforeman/puppet-puppet

commit 123d226b4120a684a6f8848b4e17771085a5c612

Add configuration support for the jvm settings when using puppetserver. (Closes GH-274)

manifests/server/puppetserver.pp
@@ -1,0 +1,60 @@
+# == Class: puppet::server::puppetserver
+#
+# Configures the puppetserver jvm configuration file using augeas.
+#
+# === Parameters:
+#
+# * `java_bin`
+# Path to the java executable to use
+#
+# * `config`
+# Path to the jvm configuration file.
+# This file is usually either /etc/default/puppetserver or
+# /etc/sysconfig/puppetserver depending on your *nix flavor.
+#
+# * `jvm_min_heap_size`
+# Translates into the -Xms option and is added to the JAVA_ARGS
+#
+# * `jvm_max_heap_size`
+# Translates into the -Xmx option and is added to the JAVA_ARGS
+#
+# * `jvm_extra_args`
+# Custom options to pass through to the java binary. These get added to
+# the end of the JAVA_ARGS variable
+#
+#
+# === Example
+#
+# @example
+#
+#   # configure memory for java < 8
+#   class {'::puppet::server::puppetserver':
+#     jvm_min_heap_size => '1G',
+#     jvm_max_heap_size => '3G',
+#     jvm_extra_args    => '-XX:MaxPermSize=256m',
+#   }
+#
+class puppet::server::puppetserver (
+  $java_bin          = $::puppet::server_jvm_java_bin,
+  $config            = $::puppet::server_jvm_config,
+  $jvm_min_heap_size = $::puppet::server_jvm_min_heap_size,
+  $jvm_max_heap_size = $::puppet::server_jvm_max_heap_size,
+  $jvm_extra_args    = $::puppet::server_jvm_extra_args,
+) {
+
+  $puppetserver_package = pick($::puppet::server_package, 'puppetserver')
+
+  $jvm_cmd_arr = ["-Xms${jvm_min_heap_size}", "-Xmx${jvm_max_heap_size}", $jvm_extra_args]
+  $jvm_cmd = strip(join(flatten($jvm_cmd_arr),' '))
+
+  augeas {'puppet::server::puppetserver::jvm':
+    lens    => 'Shellvars.lns',
+    incl    => $config,
+    context => "/files${config}",
+    changes => [
+      "set JAVA_ARGS '\"${jvm_cmd}\"'",
+      "set JAVA_BIN ${java_bin}",
+    ],
+  }
+
+}

manifests/init.pp
@@ -363,6 +363,19 @@
 #                                  the apache vhost to set up a proxy for all
 #                                  certificates pointing to the value.
 #
+# $server_jvm_java_bin::           Set the default java to use.
+#
+# $server_jvm_config::             Specify the puppetserver jvm configuration file.
+#
+# $server_jvm_min_heap_size::      Specify the minimum jvm heap space.
+#
+# $server_jvm_max_heap_size::      Specify the maximum jvm heap space.
+#
+# $server_jvm_extra_args::         Additional java options to pass through.
+#                                  This can be used for Java versions prior to
+#                                  Java 8 to specify the max perm space to use:
+#                                  For example: '-XX:MaxPermSpace=128m'.
+#
 # $allow_any_crl_auth::            Allow any authentication for the CRL. This
 #                                  is needed on the puppet CA to accept clients
 #                                  from a the puppet CA proxy.
@@ -506,6 +519,12 @@ class puppet (
   $server_puppetdb_swf           = $puppet::params::server_puppetdb_swf,
   $server_parser                 = $puppet::params::server_parser,
   $server_environment_timeout    = $puppet::params::server_environment_timeout,
+  $server_jvm_java_bin           = $puppet::params::server_jvm_java_bin,
+  $server_jvm_config             = $puppet::params::server_jvm_config,
+  $server_jvm_min_heap_size      = $puppet::params::server_jvm_min_heap_size,
+  $server_jvm_max_heap_size      = $puppet::params::server_jvm_max_heap_size,
+  $server_jvm_extra_args         = $puppet::params::server_jvm_extra_args,
+
 ) inherits puppet::params {
 
   validate_bool($listen)
@@ -568,6 +587,11 @@ class puppet (
 
   if $manage_packages != true and $manage_packages != false {
     validate_re($manage_packages, '^(server|agent)$')
+  }
+
+  if $server_implementation == 'puppetserver' {
+    validate_re($server_jvm_min_heap_size, '^[0-9]+[kKmMgG]$')
+    validate_re($server_jvm_max_heap_size, '^[0-9]+[kKmMgG]$')
   }
 
   include ::puppet::config

manifests/params.pp
@@ -284,4 +284,17 @@ class puppet::params {
 
   # Timeout for cached environments, changed in puppet 3.7.x
   $server_environment_timeout = undef
+
+  # puppet server configuration file
+  $server_jvm_config = $::osfamily ? {
+    'RedHat' => '/etc/sysconfig/puppetserver',
+    'Debian' => '/etc/default/puppetserver',
+    default  => '/etc/default/puppetserver',
+  }
+
+  $server_jvm_java_bin = '/usr/bin/java'
+  $server_jvm_min_heap_size = '2G'
+  $server_jvm_max_heap_size = '2G'
+  $server_jvm_extra_args = '' # lint:ignore:empty_string_assignment
+
 }

manifests/server/config.pp
@@ -5,6 +5,12 @@ class puppet::server::config inherits puppet::config {
     class { '::puppet::server::passenger': } -> Class['puppet::server::config']
   }
 
+  if $puppet::server_implementation == 'puppetserver' {
+    anchor {'::puppet::server::puppetserver_start': } ->
+    class { '::puppet::server::puppetserver': } ~>
+    anchor {'::puppet::server::puppetserver_end': }
+  }
+
   # Mirror the relationship, as defined() is parse-order dependent
   # Ensures puppetmasters certs are generated before the proxy is needed
   if defined(Class['foreman_proxy::config']) and $foreman_proxy::ssl {

spec/classes/puppet_init_spec.rb
@@ -102,12 +102,31 @@ describe 'puppet' do
         [
           'dir', 'logdir', 'rundir'
         ].each do |d|
-        context "when #{d} => './somedir'" do
-          let(:params) {{ d => './somedir'}}
-          it { should raise_error(Puppet::Error, /is not an absolute path/) }
+          context "when #{d} => './somedir'" do
+            let(:params) {{ d => './somedir'}}
+            it { should raise_error(Puppet::Error, /is not an absolute path/) }
+          end
         end
       end
-    end
+
+      describe 'when an invalid jvm size value is given' do
+        context "when server_jvm_min_heap_size => 'x4m'" do
+          let (:params) {{
+            :server_jvm_min_heap_size => 'x4m',
+            :server_jvm_max_heap_size => '2G',
+            :server_implementation    => 'puppetserver',
+          }}
+          it { should raise_error(Puppet::Error, /does not match "\^\[0-9\]\+\[kKmMgG\]\$"/) }
+        end
+        context "when server_jvm_max_heap_size => 'x4m'" do
+          let (:params) {{
+            :server_jvm_max_heap_size => 'x4m',
+            :server_jvm_min_heap_size => '2G',
+            :server_implementation    => 'puppetserver',
+          }}
+          it { should raise_error(Puppet::Error, /does not match "\^\[0-9\]\+\[kKmMgG\]\$"/) }
+        end
+      end
   end
 
   context 'on Windows' do

spec/classes/puppet_server_puppetserver_spec.rb
@@ -1,0 +1,58 @@
+require 'spec_helper'
+
+describe 'puppet::server::puppetserver' do
+  let(:default_params) do {
+    :java_bin          => '/usr/bin/java',
+    :config            => '/etc/default/puppetserver',
+    :jvm_min_heap_size => '2G',
+    :jvm_max_heap_size => '2G',
+    :jvm_extra_args    => '',
+  } end
+
+  describe 'with default parameters' do
+    let(:params) { default_params }
+    it { should contain_augeas('puppet::server::puppetserver::jvm').
+            with_changes([
+              'set JAVA_ARGS \'"-Xms2G -Xmx2G"\'',
+              'set JAVA_BIN /usr/bin/java',
+            ]).
+            with_context('/files/etc/default/puppetserver').
+            with_incl('/etc/default/puppetserver').
+            with_lens('Shellvars.lns').
+            with({})
+    }
+  end
+
+  describe 'with extra_args parameter' do
+    let :params do
+      default_params.merge({
+        :jvm_extra_args => ['-XX:foo=bar', '-XX:bar=foo'],
+      })
+    end
+
+    it { should contain_augeas('puppet::server::puppetserver::jvm').
+            with_changes([
+              'set JAVA_ARGS \'"-Xms2G -Xmx2G -XX:foo=bar -XX:bar=foo"\'',
+              'set JAVA_BIN /usr/bin/java',
+            ]).
+            with_context('/files/etc/default/puppetserver').
+            with_incl('/etc/default/puppetserver').
+            with_lens('Shellvars.lns').
+            with({})
+    }
+  end
+
+  describe 'with jvm_config file parameter' do
+    let :params do
+      default_params.merge({
+        :config => '/etc/custom/puppetserver',
+      })
+    end
+    it { should contain_augeas('puppet::server::puppetserver::jvm').
+            with_context('/files/etc/custom/puppetserver').
+            with_incl('/etc/custom/puppetserver').
+            with_lens('Shellvars.lns').
+            with({})
+    }
+  end
+end

spec/classes/puppet_server_spec.rb
@@ -104,6 +104,7 @@ describe 'puppet::server' do
         with_puppetmaster(nil).
         with_puppetserver(true)
     end
+    it { should contain_class('puppet::server::puppetserver') }
     it { should contain_package('puppetserver') }
   end
 

commit 01fd3ccd8d24280163a4f3ed1a973fbdb9dde21c

Remove unused variable in puppetserver.pp
Fixes: 123d226b4120a684a6f8848b4e17771085a5c612

manifests/server/puppetserver.pp
@@ -147,8 +147,6 @@ class puppet::server::puppetserver (
     fail('puppetserver <6.15.0 is not supported by this module version')
   }
 
-  $puppetserver_package = pick($puppet::server::package, 'puppetserver')
-
   $jvm_cmd_arr = ["-Xms${jvm_min_heap_size}", "-Xmx${jvm_max_heap_size}", $jvm_extra_args]
   $jvm_cmd = strip(join(flatten($jvm_cmd_arr), ' '))
 