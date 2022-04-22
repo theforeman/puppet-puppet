afe6cc0520fdefa645cd7421781aea75ddbbc5bc
Daniel Fernández <daferoes@gmail.com>
2022-04-22 15:43:38 +0200

    Add Array of Integers as valid type for run_minute

--- manifests/init.pp
+++ manifests/init.pp
@@ -589,7 +589,7 @@ class puppet (
   Boolean $usecacheonfailure = $puppet::params::usecacheonfailure,
   Enum['cron', 'service', 'systemd.timer', 'none', 'unmanaged'] $runmode = $puppet::params::runmode,
   Optional[Integer[0,23]] $run_hour = undef,
-  Optional[Integer[0,59]] $run_minute = undef,
+  Variant[Integer[0,59], Array[Integer[0,59]], Undef] $run_minute = undef,
   Array[Enum['cron', 'service', 'systemd.timer', 'none']] $unavailable_runmodes = $puppet::params::unavailable_runmodes,
   Optional[String] $cron_cmd = $puppet::params::cron_cmd,
   Optional[String] $systemd_cmd = $puppet::params::systemd_cmd,
