8a45aa7732a63fee3886a461e4eee662ec19dc70
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-05-09 11:34:25 +0200

    Drop Ubuntu 16.04, Debian 9, Fedora 31, SLES 11, Windows 8; add F36, EL9

--- spec/support/acceptance/puppetserver.rb
+++ spec/support/acceptance/puppetserver.rb
@@ -4,6 +4,8 @@ def unsupported_puppetserver
     true
   when 'Fedora'
     true
+  when 'Debian'
+    host_inventory['facter']['os']['release']['major'] == '11'
   when 'Ubuntu'
     ENV['BEAKER_PUPPET_COLLECTION'] == 'puppet6' && host_inventory['facter']['os']['release']['major'] == '20.04'
   end
