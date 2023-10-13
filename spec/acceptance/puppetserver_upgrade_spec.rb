79189141a3656410d3c324377358a1a2b3d4809f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-10-13 18:05:34 +0200

    Avoid unless: kwarg on upgrade acceptance test
    With the unless keyword argument on a describe block the content is
    still parsed by Ruby. This means some functions are still executed. It
    also ends up triggering the unsupported version exception on Archlinux
    because the collection is unrecognized, even though the test really
    isn't run. By using the real Ruby unless statement this is all avoided.

--- spec/acceptance/puppetserver_upgrade_spec.rb
+++ spec/acceptance/puppetserver_upgrade_spec.rb
@@ -1,84 +1,86 @@
 require 'spec_helper_acceptance'
 
-describe 'Scenario: minor version upgrade', unless: unsupported_puppetserver do
-  before(:all) do
-    if check_for_package(default, 'puppetserver')
-      on default, 'puppet resource package puppetserver ensure=purged'
-      on default, 'rm -rf /etc/sysconfig/puppetserver /etc/puppetlabs/puppetserver'
-      on default, 'rm -rf /etc/puppetlabs/puppet/ssl'
+unless unsupported_puppetserver
+  describe 'Scenario: minor version upgrade' do
+    before(:all) do
+      if check_for_package(default, 'puppetserver')
+        on default, 'puppet resource package puppetserver ensure=purged'
+        on default, 'rm -rf /etc/sysconfig/puppetserver /etc/puppetlabs/puppetserver'
+        on default, 'rm -rf /etc/puppetlabs/puppet/ssl'
+      end
+
+      # puppetserver won't start with low memory
+      memoryfree_mb = fact('memoryfree_mb').to_i
+      raise 'At least 256MB free memory required' if memoryfree_mb < 256
     end
 
-    # puppetserver won't start with low memory
-    memoryfree_mb = fact('memoryfree_mb').to_i
-    raise 'At least 256MB free memory required' if memoryfree_mb < 256
-  end
+    case ENV['BEAKER_PUPPET_COLLECTION']
+    when 'puppet7'
+      from_version = '7.0.0'
+      to_version = '7.2.0'
+    else
+      raise 'Unsupported Puppet collection'
+    end
 
-  case ENV['BEAKER_PUPPET_COLLECTION']
-  when 'puppet7'
-    from_version = '7.0.0'
-    to_version = '7.2.0'
-  else
-    raise 'Unsupported Puppet collection'
-  end
+    case fact('osfamily')
+    when 'Debian'
+      from_version_exact = "#{from_version}-1#{fact('os.distro.codename')}"
+      to_version_exact = "#{to_version}-1#{fact('os.distro.codename')}"
+    else
+      from_version_exact = from_version
+      to_version_exact = to_version
+    end
 
-  case fact('osfamily')
-  when 'Debian'
-    from_version_exact = "#{from_version}-1#{fact('os.distro.codename')}"
-    to_version_exact = "#{to_version}-1#{fact('os.distro.codename')}"
-  else
-    from_version_exact = from_version
-    to_version_exact = to_version
-  end
+    context "install #{from_version}" do
+      it_behaves_like 'an idempotent resource' do
+        let(:manifest) do
+          <<-EOS
+          class { 'puppet':
+            server         => true,
+            server_version => '#{from_version_exact}',
+          }
+          EOS
+        end
+      end
 
-  context "install #{from_version}" do
-    it_behaves_like 'an idempotent resource' do
-      let(:manifest) do
-        <<-EOS
-        class { 'puppet':
-          server         => true,
-          server_version => '#{from_version_exact}',
-        }
-        EOS
+      describe command('puppetserver --version') do
+        its(:stdout) { is_expected.to match("puppetserver version: #{from_version}\n") }
+      end
+
+      describe service('puppetserver') do
+        it { is_expected.to be_enabled }
+        it { is_expected.to be_running }
+      end
+
+      describe port('8140') do
+        it { is_expected.to be_listening }
       end
     end
 
-    describe command('puppetserver --version') do
-      its(:stdout) { is_expected.to match("puppetserver version: #{from_version}\n") }
-    end
-
-    describe service('puppetserver') do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
-    end
-
-    describe port('8140') do
-      it { is_expected.to be_listening }
-    end
-  end
-
-  context "upgrade to #{to_version}" do
-    it_behaves_like 'an idempotent resource' do
-      let(:manifest) do
-        <<-EOS
-        class { 'puppet':
-          server         => true,
-          server_version => '#{to_version_exact}',
-        }
-        EOS
+    context "upgrade to #{to_version}" do
+      it_behaves_like 'an idempotent resource' do
+        let(:manifest) do
+          <<-EOS
+          class { 'puppet':
+            server         => true,
+            server_version => '#{to_version_exact}',
+          }
+          EOS
+        end
       end
-    end
 
-    describe command('puppetserver --version') do
-      its(:stdout) { is_expected.to match("puppetserver version: #{to_version}\n") }
-    end
+      describe command('puppetserver --version') do
+        its(:stdout) { is_expected.to match("puppetserver version: #{to_version}\n") }
+      end
 
-    describe service('puppetserver') do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
-    end
+      describe service('puppetserver') do
+        it { is_expected.to be_enabled }
+        it { is_expected.to be_running }
+      end
 
-    describe port('8140') do
-      it { is_expected.to be_listening }
+      describe port('8140') do
+        it { is_expected.to be_listening }
+      end
     end
   end
 end
