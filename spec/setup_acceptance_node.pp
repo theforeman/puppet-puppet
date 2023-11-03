8f0203b34e73cf0fac10ce53590b36e620ccda24
Evgeni Golov <evgeni@golov.de>
2023-11-03 10:18:42 +0100

    explicitly install cronie on other EL

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -1,8 +1,11 @@
 $packages = $facts['os']['name'] ? {
   # iproute is needed for the ss command in testing and not included in the base container
-  'Fedora' => ['cronie', 'iproute'],
-  'Ubuntu' => ['cron'],
-  default  => [],
+  'Fedora'      => ['cronie', 'iproute'],
+  'Ubuntu'      => ['cron'],
+  'AlmaLinux'   => ['cronie'],
+  'OracleLinux' => ['cronie'],
+  'Rocky'       => ['cronie'],
+  default       => [],
 }
 
 package { $packages:
