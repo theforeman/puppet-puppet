672c45f97697a1bdee91c47266069860b9d6e433
Evgeni Golov <evgeni@golov.de>
2023-11-03 09:14:14 +0100

    Add Ubuntu 22.04 support

--- spec/support/acceptance/puppetserver.rb
+++ spec/support/acceptance/puppetserver.rb
@@ -8,3 +8,9 @@ def unsupported_puppetserver
     host_inventory['facter']['os']['release']['major'] == '12'
   end
 end
+
+# These versions only have a single version (x.y.z) released so no upgrade is possible
+def unsupported_puppetserver_upgrade
+  (fact('os.family') == 'RedHat' && fact('os.release.major') == '9') ||
+    (fact('os.name') == 'Ubuntu' && fact('os.release.major') == '22.04')
+end
