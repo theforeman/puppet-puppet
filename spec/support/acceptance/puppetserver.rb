b5243548f38099e816c38ecb9be7c30170dd9642
Evgeni Golov <evgeni@golov.de>
2024-03-21 10:08:45 +0100

    test upgrades on EL9 and Ubuntu 22.04

--- spec/support/acceptance/puppetserver.rb
+++ spec/support/acceptance/puppetserver.rb
@@ -9,8 +9,7 @@ def unsupported_puppetserver
   end
 end
 
-# These versions only have a single version (x.y.z) released so no upgrade is possible
 def unsupported_puppetserver_upgrade
-  (fact('os.family') == 'RedHat' && fact('os.release.major') == '9') ||
-    (fact('os.name') == 'Ubuntu' && fact('os.release.major') == '22.04')
+  # currently none
+  false
 end
