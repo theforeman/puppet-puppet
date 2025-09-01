15253e83ce8b8c0dde32aefeb6e01b3b63135c51
Matt Darcy <matt@projecthugo.co.uk>
2025-09-01 21:36:37 +0000

    define facter parameters for use

--- manifests/params.pp
+++ manifests/params.pp
@@ -34,6 +34,9 @@ class puppet::params {
   $dns_alt_names       = []
   $use_srv_records     = false
   $agent_default_schedules = false
+  $facter_blocklist    = undef
+  $facter_cachelist    = undef
+  $cache_ttl           = '1 day',
 
   $srv_domain = fact('networking.domain')
 
