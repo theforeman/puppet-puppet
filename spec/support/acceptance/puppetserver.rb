ce2f9fab21e592e75849693c9f6c78c664ca2d99
Tim Meusel <tim@bastelfreak.de>
2025-10-10 11:46:44 +0200

    Enable acceptance testing for Debian 12 puppetserver
    initially there was no puppetserver package for Debian 12, so the tests
    were disabled. Sadly there's just a single package:
    
    https://apt.puppet.com/pool/bookworm/puppet8/p/puppetserver/index.html
    
    So we cannot test puppetserver upgrades.

--- spec/support/acceptance/puppetserver.rb
+++ spec/support/acceptance/puppetserver.rb
@@ -4,12 +4,16 @@ def unsupported_puppetserver
     true
   when 'Fedora'
     true
-  when 'Debian'
-    host_inventory['facter']['os']['release']['major'] == '12'
   end
 end
 
 def unsupported_puppetserver_upgrade
-  # currently none
-  false
+  case host_inventory['facter']['os']['name']
+  when 'Archlinux'
+    true
+  when 'Fedora'
+    true
+  when 'Debian'
+    host_inventory['facter']['os']['release']['major'] == '12'
+  end
 end
