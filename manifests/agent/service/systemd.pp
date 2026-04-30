86b1c598d8adad0103260d355fea63d3ecefb6f5
Tim Meusel <tim@bastelfreak.de>
2026-04-30 14:31:22 +0200

    puppet-lint: fix params_not_optional_with_undef

--- manifests/agent/service/systemd.pp
+++ manifests/agent/service/systemd.pp
@@ -3,7 +3,7 @@
 class puppet::agent::service::systemd (
   Boolean                 $enabled                             = false,
   Optional[Integer[0,23]] $hour                                = undef,
-  Variant[Integer[0,59], Array[Integer[0,59]], Undef] $minute  = undef,
+  Optional[Variant[Integer[0,59], Array[Integer[0,59]]]] $minute = undef,
   Optional[String[1]] $timezone                                = undef,
 ) {
   unless $puppet::runmode == 'unmanaged' or 'systemd.timer' in $puppet::unavailable_runmodes {
